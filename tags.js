3c7b81a39794f95a4df5772f066675032858098d
Sidney Lima Filho <sidneylimafilho@outlook.com>
2012-12-10 15:19:42 -0200

    add tags in post

--- tags.js
+++ tags.js
@@ -1,13 +1,15 @@
 ---
 layout: nil
 ---
-var tags = {};
-{% for post in site.posts %}
-   {% for tag in post.tags %}
-      var tag = tags["{{tag}}"] = tags["{{tag}}"] || {count:0, posts:[]};
-      tag.count++;
-      tag.posts.push({
-      	title: "{{post.title}}"
-      });
-   {% endfor %}
-{% endfor %}
+var tag,tags = {};
+
+{% for post in site.posts %} {% for tag in post.tags %}
+tag = tags["{{tag}}"] = tags["{{tag}}"] || {count:0, posts:[]};
+tag.count++;
+tag.posts.push({
+	title: "{{post.title}}",
+	excerpt: "{{post.excerpt}}",
+	date: "{{post.date}}",
+	tags:[ {% for posttag in post.tags %}"{{posttag}}",{% endfor %}	false]
+});
+{% endfor %} {% endfor %}
