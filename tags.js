dbbb2eeeb3d90ebf9d9984d684f9f150584d184e
Sidney Lima Filho <sidneylimafilho@outlook.com>
2012-12-10 15:32:35 -0200

    date parse ruby to js

--- tags.js
+++ tags.js
@@ -4,12 +4,12 @@ layout: nil
 var tag,tags = {};
 
 {% for post in site.posts %} {% for tag in post.tags %}
-tag = tags["{{tag}}"] = tags["{{tag}}"] || {count:0, posts:[]};
+tag = tags["{{tag}}"] = (tags["{{tag}}"] || {count:0, posts:[]});
 tag.count++;
 tag.posts.push({
 	title: "{{post.title}}",
 	excerpt: "{{post.excerpt}}",
-	date: "{{post.date}}",
+	date: Date.parse("{{post.date}}"),
 	tags:[ {% for posttag in post.tags %}"{{posttag}}",{% endfor %}	false]
 });
 {% endfor %} {% endfor %}
