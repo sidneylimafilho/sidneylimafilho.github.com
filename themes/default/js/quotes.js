9840d89280e298d7702c988678f778af6c5c0d0c
Sidney Lima Filho <sidneylimafilho@outlook.com>
2013-01-24 19:36:35 -0200

    alpha version

--- themes/default/js/quotes.js
+++ themes/default/js/quotes.js
@@ -1,42 +1,54 @@
----
-layout: nil
----
-quotes = [
-    {
-        author: "Sidney Lima Filho",
-        quote: "Os resultados consistem no feedback que recebemos sobre a qualidade de nossa tomada de decisões."
-    } , {
-        author: "Garry Kasparov",
-        quote: "O que interessa é desenvolver um sistema de agressividade controlada, a fim de nos separarmos dos inertes. Agressividade, nesse contexto, significa dinamismo, coragem e muita vontade de agir."
-    } , {
-        author: "Sidney Lima Filho",
-        quote: "Pessoas medíocres acreditam que é melhor um mal conhecido do que um bem desconhecido."
-    } , {
-        author: "Margaret Thatcher",
-        quote: "Nunca tome uma decisão até que ela seja necessária."
-    } , {
-        author: "Ditado Russo",
-        quote: "Até as balas temem os corajosos."
-    } , {
-        author: "Garry Kasparov",
-        quote: "Só mude uma estratégia se o ambiente nela inserida mudar."
-    } , {
-        author: "Sidney Lima Filho",
-        quote: "Uma troca sempre tem inconveniência, mesmo que seja de pior para melhor."
-    } , {
-        author: "Garry Kasparov",
-        quote: "Saber porque vencemos é mais importante do que saber o porque perdemos."
-    } , {
-        author: "Goethe",
-        quote: "Saber não basta, devemos aplicar. Desejar não basta, devemos fazer."
-    } , {
-        author: "Garry Kasparov",
-        quote: "A maioria das pessoas só aprende o suficiente para usar. As outras usa suficientemente o que aprendeu."
-    } , {
-        author: "Garry Kasparov",
-        quote: "Uma ameaça geralmente é tão forte quanto sua execução."
-    } , {
-        author: "Sidney Lima Filho",
-        quote: "Quando as mentes brilhantes tornam-se especialistas, a liderança é relegado as mentes mais fracas."
-    }
-];
+quotes = [{
+    author: "Sidney Lima Filho",
+    title: "APRENDA COMO FUNCIONA",
+    text: "Desenvolver Software é a arte de cultivar a informação de forma que ela cresça naturalmente através de mãos distintas.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Sidney Lima Filho",
+    text: "Os resultados consistem no feedback que recebemos sobre a qualidade de nossa tomada de decisões.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Garry Kasparov",
+    text: "O que interessa é desenvolver um sistema de agressividade controlada, a fim de nos separarmos dos inertes. Agressividade, nesse contexto, significa dinamismo, coragem e muita vontade de agir.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Sidney Lima Filho",
+    text: "Pessoas medíocres acreditam que é melhor um mal conhecido do que um bem desconhecido.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Margaret Thatcher",
+    text: "Nunca tome uma decisão até que ela seja necessária.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Ditado Russo",
+    text: "Até as balas temem os corajosos.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Garry Kasparov",
+    text: "Só mude uma estratégia se o ambiente nela inserida mudar.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Sidney Lima Filho",
+    text: "Uma troca sempre tem inconveniência, mesmo que seja de pior para melhor.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Garry Kasparov",
+    text: "Saber porque vencemos é mais importante do que saber o porque perdemos.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Goethe",
+    text: "Saber não basta, devemos aplicar. Desejar não basta, devemos fazer.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Garry Kasparov",
+    text: "A maioria das pessoas só aprende o suficiente para usar. As outras usa suficientemente o que aprendeu.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Garry Kasparov",
+    text: "Uma ameaça geralmente é tão forte quanto sua execução.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}, {
+    author: "Sidney Lima Filho",
+    text: "Quando as mentes brilhantes tornam-se especialistas, a liderança é relegado as mentes mais fracas.",
+    banner: "nurture_seedling_in_hands_low_level.jpg"
+}];
