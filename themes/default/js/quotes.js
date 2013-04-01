f2d7f1969e060e1a903238654473f664830fabc8
Sidney Lima Filho <sidneylimafilho@outlook.com>
2013-04-01 02:04:45 -0300

    Adicionado banner sidney-meditation.jpg e alterações de css

--- themes/default/js/quotes.js
+++ themes/default/js/quotes.js
@@ -3,11 +3,11 @@ quotes = [{
     author: "Sidney Lima Filho",
     title: "APRENDA COMO FUNCIONA",
     text: "Desenvolver Software é a arte de cultivar a informação de forma que ela cresça naturalmente através de mãos distintas.",
-    banner: "nurture_seedling_in_hands_low_level.jpg"
+    banner: "craftsmanship-cover.jpg"
 }, {
     author: "Sidney Lima Filho",
     text: "Os resultados consistem no feedback que recebemos sobre a qualidade de nossa tomada de decisões.",
-    banner: "nurture_seedling_in_hands_low_level.jpg"
+    banner: "sidney-meditation.jpg"
 }, {
     author: "Garry Kasparov",
     text: "O que interessa é desenvolver um sistema de agressividade controlada, a fim de nos separarmos dos inertes. Agressividade, nesse contexto, significa dinamismo, coragem e muita vontade de agir.",
@@ -15,7 +15,7 @@ quotes = [{
 }, {
     author: "Sidney Lima Filho",
     text: "Pessoas medíocres acreditam que é melhor um mal conhecido do que um bem desconhecido.",
-    banner: "nurture_seedling_in_hands_low_level.jpg"
+    banner: "sidney-meditation.jpg"
 }, {
     author: "Margaret Thatcher",
     text: "Nunca tome uma decisão até que ela seja necessária.",
@@ -31,7 +31,7 @@ quotes = [{
 }, {
     author: "Sidney Lima Filho",
     text: "Uma troca sempre tem inconveniência, mesmo que seja de pior para melhor.",
-    banner: "nurture_seedling_in_hands_low_level.jpg"
+    banner: "sidney-meditation.jpg"
 }, {
     author: "Garry Kasparov",
     text: "Saber porque vencemos é mais importante do que saber o porque perdemos.",
@@ -39,7 +39,7 @@ quotes = [{
 }, {
     author: "Goethe",
     text: "Saber não basta, devemos aplicar. Desejar não basta, devemos fazer.",
-    banner: "nurture_seedling_in_hands_low_level.jpg"
+    banner: "road.jpg"
 }, {
     author: "Garry Kasparov",
     text: "A maioria das pessoas só aprende o suficiente para usar. As outras usa suficientemente o que aprendeu.",
@@ -60,7 +60,7 @@ $(function() {
     var $banner = $("banner");
     $banner.find(".excerpt h1").text(q.author);
     $banner.find(".excerpt h2").text(q.text);
-    $("banner, footer").css({
+    $("banner").css({
         background: 'url(/themes/default/img/banner/' + q.banner + ')'
     });
 })
