1ca31bd709ffa9c19c7e9bb3740d0c9342d71975
Sidney Lima Filho <sidneylimafilho@outlook.com>
2013-04-02 13:26:08 -0300

    #1: Adicionada busca do Google

--- themes/default/js/user.js
+++ themes/default/js/user.js
@@ -6,6 +6,60 @@ layout: nil
 User = {
     items: [],
     name: "sidneylimafilho",
+    quotes : [{
+        author: "Sidney Lima Filho",
+        title: "APRENDA COMO FUNCIONA",
+        text: "Desenvolver Software é a arte de cultivar a informação de forma que ela cresça naturalmente através de mãos distintas.",
+        banner: "craftsmanship-cover.jpg"
+    }, {
+        author: "Sidney Lima Filho",
+        text: "Os resultados consistem no feedback que recebemos sobre a qualidade de nossa tomada de decisões.",
+        banner: "sidney-meditation.jpg"
+    }, {
+        author: "Garry Kasparov",
+        text: "O que interessa é desenvolver um sistema de agressividade controlada, a fim de nos separarmos dos inertes. Agressividade, nesse contexto, significa dinamismo, coragem e muita vontade de agir.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }, {
+        author: "Sidney Lima Filho",
+        text: "Pessoas medíocres acreditam que é melhor um mal conhecido do que um bem desconhecido.",
+        banner: "sidney-meditation.jpg"
+    }, {
+        author: "Margaret Thatcher",
+        text: "Nunca tome uma decisão até que ela seja necessária.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }, {
+        author: "Ditado Russo",
+        text: "Até as balas temem os corajosos.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }, {
+        author: "Garry Kasparov",
+        text: "Só mude uma estratégia se o ambiente nela inserida mudar.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }, {
+        author: "Sidney Lima Filho",
+        text: "Uma troca sempre tem inconveniência, mesmo que seja de pior para melhor.",
+        banner: "sidney-meditation.jpg"
+    }, {
+        author: "Garry Kasparov",
+        text: "Saber porque vencemos é mais importante do que saber o porque perdemos.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }, {
+        author: "Goethe",
+        text: "Saber não basta, devemos aplicar. Desejar não basta, devemos fazer.",
+        banner: "road.jpg"
+    }, {
+        author: "Garry Kasparov",
+        text: "A maioria das pessoas só aprende o suficiente para usar. As outras usa suficientemente o que aprendeu.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }, {
+        author: "Garry Kasparov",
+        text: "Uma ameaça geralmente é tão forte quanto sua execução.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }, {
+        author: "Margaret Thatcher",
+        text: "Quando as mentes brilhantes tornam-se especialistas, a liderança é relegado as mentes mais fracas.",
+        banner: "nurture_seedling_in_hands_low_level.jpg"
+    }],
     mail: {
         name: "sidneylimafilho",
         getUrl: function() {
